2c99fd2fdc7b4f9c79f827bf7402e8e9d60de8af
praisechan <wncks1414@naver.com>
2025-09-27 05:12:19 +0000

    run_files commit

--- run_files/dynamic_verify_test/3stage_pg19_profile_2step_qwen_0.1.sh
+++ run_files/dynamic_verify_test/3stage_pg19_profile_2step_qwen_0.1.sh
@@ -1,10 +1,13 @@
 # ############PG19#############
 export CLUSTER_SIZE=32
-python tests/dynamic_verify_test/run_2step_profile.py --model_name qwen2.5-14b --gamma1 4 --gamma2 32 --budget1 0.1 --prefix_len 8224 --dataset pg19
-python tests/dynamic_verify_test/run_2step_profile.py --model_name qwen2.5-14b --gamma1 4 --gamma2 32 --budget1 0.1 --prefix_len 16416 --dataset pg19
-python tests/dynamic_verify_test/run_2step_profile.py --model_name qwen2.5-14b --gamma1 4 --gamma2 32 --budget1 0.1 --prefix_len 32800 --dataset pg19
-# python tests/dynamic_verify_test/run_2step_profile.py --model_name qwen2.5-14b --gamma1 4 --gamma2 32 --budget1 0.1 --prefix_len 65568 --dataset pg19
-python tests/dynamic_verify_test/run_2step_profile.py --model_name qwen2.5-14b --gamma1 4 --gamma2 32 --budget1 0.4 --prefix_len 8224 --dataset pg19
-python tests/dynamic_verify_test/run_2step_profile.py --model_name qwen2.5-14b --gamma1 4 --gamma2 32 --budget1 0.4 --prefix_len 16416 --dataset pg19
-python tests/dynamic_verify_test/run_2step_profile.py --model_name qwen2.5-14b --gamma1 4 --gamma2 32 --budget1 0.4 --prefix_len 32800 --dataset pg19
-# python tests/dynamic_verify_test/run_2step_profile.py --model_name qwen2.5-14b --gamma1 4 --gamma2 32 --budget1 0.4 --prefix_len 65568 --dataset pg19
+# python tests/dynamic_verify_test/run_2step_profile.py --model_name qwen2.5-14b --gamma1 4 --gamma2 32 --budget1 0.1 --prefix_len 8224 --dataset pg19
+# python tests/dynamic_verify_test/run_2step_profile.py --model_name qwen2.5-14b --gamma1 4 --gamma2 32 --budget1 0.1 --prefix_len 16416 --dataset pg19
+# python tests/dynamic_verify_test/run_2step_profile.py --model_name qwen2.5-14b --gamma1 4 --gamma2 32 --budget1 0.1 --prefix_len 32800 --dataset pg19
+python tests/dynamic_verify_test/run_2step_profile.py --model_name qwen2.5-14b --gamma1 4 --gamma2 32 --budget1 0.1 --prefix_len 65568 --dataset pg19
+# python tests/dynamic_verify_test/run_2step_profile.py --model_name qwen2.5-14b --gamma1 4 --gamma2 32 --budget1 0.4 --prefix_len 8224 --dataset pg19
+# python tests/dynamic_verify_test/run_2step_profile.py --model_name qwen2.5-14b --gamma1 4 --gamma2 32 --budget1 0.4 --prefix_len 16416 --dataset pg19
+# python tests/dynamic_verify_test/run_2step_profile.py --model_name qwen2.5-14b --gamma1 4 --gamma2 32 --budget1 0.4 --prefix_len 32800 --dataset pg19
+python tests/dynamic_verify_test/run_2step_profile.py --model_name qwen2.5-14b --gamma1 4 --gamma2 32 --budget1 0.4 --prefix_len 65568 --dataset pg19
+
+python tests/dynamic_verify_test/run_2step_profile.py --model_name qwen2.5-32b --gamma1 4 --gamma2 32 --budget1 0.1 --prefix_len 65568 --dataset pg19
+python tests/dynamic_verify_test/run_2step_profile.py --model_name qwen2.5-32b --gamma1 4 --gamma2 32 --budget1 0.4 --prefix_len 65568 --dataset pg19
